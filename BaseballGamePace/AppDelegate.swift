76d266019ca9fc97329871e6774bb750440c976a
Craig Dietrich <craigdietrich@gmail.com>
2019-09-27 16:54:53 -0700

    Initial Commit

---
+++ BaseballGamePace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BaseballGamePace
+//
+//  Created by Craig Dietrich on 9/27/19.
+//  Copyright © 2019 craigdietrich.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BaseballGamePace/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaseballGamePace
+//
+//  Created by Craig Dietrich on 9/27/19.
+//  Copyright © 2019 craigdietrich.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
